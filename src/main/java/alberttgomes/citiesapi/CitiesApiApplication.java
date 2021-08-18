fff852bdd7ea2a398fbe56cb7c054437328e0c42
Albert Gomes <albertgomes1995@hotmail.com>
2021-08-18 16:03:28 -0300

    Update CitiesApiApplication.java

--- src/main/java/alberttgomes/citiesapi/CitiesApiApplication.java
+++ src/main/java/alberttgomes/citiesapi/CitiesApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CitiesApiApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(CitiesApiApplication.class, args);
 	}
 
